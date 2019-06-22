6c6281b0563bab7a475b425e7450bd2beedafbe1
Jorge Smulevici <jsmulevici@gmail.com>
2019-06-22 21:06:04 +0300

    add programs setup

---
+++ programs.sh
@@ -0,0 +1,24 @@
+brew cask install sublime-text
+brew cask install google-chrome
+brew cask install dropbox
+brew cask install zoomus
+brew cask install skype
+brew cask install whatsapp
+brew cask install vlc
+brew cask install the-unarchiver
+brew cask install spotify
+brew cask install iterm2
+brew install awscli
+brew cask install docker
+brew cask install intellij-idea
+brew cask install sourcetree
+brew cask install postman
+brew cask install teamviewer	
+brew cask install evernote
+brew install postgresql
+brew cask install pgadmin4
+brew install mongodb
+brew install redis
+brew cask install libreoffice
+brew cask install 
+brew cask install 
