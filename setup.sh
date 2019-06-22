a59cb697a34bc7edb793b2d31efe3e40d921607e
Jorge Smulevici <jsmulevici@gmail.com>
2019-06-22 21:35:10 +0300

    add terminal enhancements to setup

--- setup.sh
+++ setup.sh
@@ -1,10 +1,12 @@
-echo "Starting set up..."
+echo "Starting setup..."
 echo "Setting up python..."
 bash ./python.sh
 echo "Setting up ruby..."
 bash ./ruby.sh
 echo "Setting up programs..."
 bash ./programs.sh
+echo "Setting up terminal enhancements..."
+bash ./aliases_and_links.sh
 echo "Setting up aliases and links..."
 bash ./aliases_and_links.sh
 echo "Setting up completed."
