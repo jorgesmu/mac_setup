40058ee18225944fe574288da3d9aea19b5be6ec
Jorge Smulevici <jsmulevici@gmail.com>
2019-06-22 21:05:51 +0300

    add ruby setup

---
+++ ruby.sh
@@ -0,0 +1,9 @@
+RUBY_VERSION=2.6.3
+echo "# Ruby" >> ~/.bash_profile
+echo >> ~/.bash_profile
+
+\curl -sSL https://get.rvm.io | bash -s stable
+echo 'source /Users/jorge/.rvm/scripts/rvm' >> ~/.bash_profile
+
+rvm install $RUBY_VERSION
+echo "rvm use $RUBY_VERSION" >> ~/.bash_profile
