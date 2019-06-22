1f5ba0a297524b2d2678cf3fe728547c4e941b36
Jorge Smulevici <jsmulevici@gmail.com>
2019-06-22 21:25:20 +0300

    add setup script

---
+++ setup.sh
@@ -0,0 +1,10 @@
+echo "Starting set up..."
+echo "Setting up python..."
+bash ./python.sh
+echo "Setting up ruby..."
+bash ./ruby.sh
+echo "Setting up programs..."
+bash ./programs.sh
+echo "Setting up aliases and links..."
+bash ./aliases_and_links.sh
+echo "Setting up completed."
